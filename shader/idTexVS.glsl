7229e06fe3be8a0cc0d4a68771ec99a9fa071012
Thorsten Trinkaus <116072811+Thorsten-Trinkaus@users.noreply.github.com>
2024-04-30 03:30:31 +0200

    Delete shader directory

--- shader/idTexVS.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-attribute vec3 vertPos;
-uniform mat4 mProj;
-uniform mat4 mView;
-uniform mat4 mWorld;
-
-void main() {
-	gl_Position = mProj * mView * mWorld * vec4(vertPos, 1.0);
-}
